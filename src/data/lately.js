a0101df7d79cf6ff69349426130b172355cf22a0
zachdexter <zsdexter05@gmail.com>
2026-03-15 10:58:22 +1100

    updated lately

--- src/data/lately.js
+++ src/data/lately.js
@@ -12,11 +12,11 @@ export const LATELY_DATA = {
     description: '',
   },
   tv: {
-    title:       'Night Agent',
+    title:       'The Rookie',
     type:        'TV Show',
     status:      'Currently Watching',
     image:       './assets/lately/tv.png',  // e.g. '/assets/lately/tv.jpg'
-    description: 'This show\'s first season hooked me, but it\'s gotten progressively worse, on third season now.',
+    description: 'Been keeping up with this show for a while, love that episodes are back to coming out weekly.',
   },
   book: {
     title:       'Run',
@@ -26,10 +26,10 @@ export const LATELY_DATA = {
     description: 'Recently finished "Dark Matter" by the same author, really loved it so trying out another one of his titles, so far pretty good!',
   },
   photos: [
+    { src: './assets/lately/photo4.JPG',  caption: 'Girlfriend recently came to visit for a week! This picture is from us climbing the Sydney Harbour Bridge, would highly recommend doing this if you\'re ever in Sydney.', date: '03/10/26', location: 'Sydney, Australia' },
     { src: './assets/lately/photo1.jpeg', caption: 'Recent trip with some friends to New Zealand! The landscape everywhere was beautiful, felt like I was living in a Windows homescreen.', date: '02/01/26', location: 'Queenstown, New Zealand' },
     { src: './assets/lately/photo2.jpeg', caption: 'Australia Day in Sydney! Picture is from the harbor just outside of the Opera House, pretty cool light show and fireworks.', date: '01/26/26', location: 'Sydney, Australia' },
     { src: './assets/lately/photo3.JPG',  caption: 'Hello Sydney!', date: '01/02/26', location: 'Sydney, Australia' },
-    { src: './assets/lately/photo4.PNG',  caption: 'Fun little outdoor climbing trip with gw club climbing (this route is called "Don\'t Fall Right", for obvious reasons).', date: '11/11/25', location: 'NW Branch, Maryland' },
   ],
   game: {
     title:       'Nothing rn :( ',
